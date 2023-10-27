c7730ca36dfdcf4ec4d4f088f69c3176ac4d3f29
4CEspX <simon@jonassonmail.se>
2023-10-27 09:36:24 +0200

    slime

--- Assets/Scripts/Entities/EnemyBlob.cs
+++ Assets/Scripts/Entities/EnemyBlob.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class EnemyBlob : MonoBehaviour
 {
+    public float speed;
+    public GameObject player;
     void Start()
     {
-
+        player = GameObject.FindWithTag("Player");
     }
 
     void Update()
     {
-
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Slime Hit Player");
+        }
+    }
+
 }
