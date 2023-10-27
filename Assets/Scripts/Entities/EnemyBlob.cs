0139062b8e26abaa068e4897983ebcf792338ba2
4CEspX <simon@jonassonmail.se>
2023-10-27 10:31:59 +0200

    a

--- Assets/Scripts/Entities/EnemyBlob.cs
+++ Assets/Scripts/Entities/EnemyBlob.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class EnemyBlob : MonoBehaviour
@@ -7,7 +8,9 @@ public class EnemyBlob : MonoBehaviour
     public float speed;
     public GameObject PlayerObj;
     Player player;
-    bool isColliding = false;
+    bool notMoving = false;
+    float followTimer = 1;
+
     void Start()
     {
         PlayerObj = GameObject.FindWithTag("Player");
@@ -16,18 +19,30 @@ public class EnemyBlob : MonoBehaviour
 
     void Update()
     {
-        if (!isColliding)
+        if (!notMoving)
         {
             transform.position = Vector3.MoveTowards(transform.position, PlayerObj.transform.position, speed * Time.deltaTime);
         }
+        else if (notMoving)
+        {
+            followTimer -= Time.deltaTime;
+        }
+        if (followTimer <= 0)
+        {
+            notMoving = false;
+            Debug.Log(followTimer);
+            followTimer = 1;
+        }
+        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("Slime Hit Player");
-            player.TakeDamage(1);
-            isColliding = true;
+            player.TakeDamage(0.5f);
+            notMoving = true;
+            
         }
     }
 }
