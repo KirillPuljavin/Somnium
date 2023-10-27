89e409b3c2ce1fd2e8a16f8bf2f16a62a3b97a8d
4CEspX <simon@jonassonmail.se>
2023-10-27 10:10:43 +0200

    blob tweeks

--- Assets/Scripts/Entities/EnemyBlob.cs
+++ Assets/Scripts/Entities/EnemyBlob.cs
@@ -7,23 +7,27 @@ public class EnemyBlob : MonoBehaviour
     public float speed;
     public GameObject PlayerObj;
     Player player;
+    bool isColliding = false;
     void Start()
     {
         PlayerObj = GameObject.FindWithTag("Player");
-        player.TakeDamage(1);
+        player = PlayerObj.GetComponent<Player>();
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, PlayerObj.transform.position, speed * Time.deltaTime);
+        if (!isColliding)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PlayerObj.transform.position, speed * Time.deltaTime);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("Slime Hit Player");
-
+            player.TakeDamage(1);
+            isColliding = true;
         }
     }
-
 }
