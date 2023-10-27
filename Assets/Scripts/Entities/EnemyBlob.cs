b15f5c9de45e207dffdb60d5d2d41bcece035671
Queereell <thelegendaryyouth@gmail.com>
2023-10-27 09:54:53 +0200

    enemy hit

--- Assets/Scripts/Entities/EnemyBlob.cs
+++ Assets/Scripts/Entities/EnemyBlob.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class EnemyBlob : MonoBehaviour
 {
     public float speed;
-    public GameObject player;
+    public GameObject PlayerObj;
+    Player player;
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        PlayerObj = GameObject.FindWithTag("Player");
+        player.TakeDamage(1);
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, PlayerObj.transform.position, speed * Time.deltaTime);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("Slime Hit Player");
+
         }
     }
 
