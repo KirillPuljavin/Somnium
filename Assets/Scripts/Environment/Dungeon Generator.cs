55217a79af1b8b8e32cf9aebf93662fc0872ca75
Queereell <thelegendaryyouth@gmail.com>
2023-11-07 14:45:39 +0100

    Dungeon Rooms

--- Assets/Scripts/Environment/Dungeon Generator.cs
+++ Assets/Scripts/Environment/Dungeon Generator.cs
@@ -9,9 +9,7 @@ public class DungeonGenerator : MonoBehaviour
     private GameObject[,] Rooms;
     private DungeonPreset currentPreset;
     public GameObject passagePrefab;
-
-    // Variables
-
+    public GameObject roomPrefab;
 
     void Start()
     {
@@ -26,16 +24,19 @@ public class DungeonGenerator : MonoBehaviour
             }
         }
 
-        // Choose passage preset
         currentPreset = new Dungeon1();
-        VisualizeDungeon();
+        // VisualizeDungeon();
 
         // Choose rooms
+        foreach (var room in currentPreset.roomsAvailable)
+        {
+            GameObject newRoom = Instantiate(roomPrefab, new Vector3(CalculateRoomPosition(room).x, CalculateRoomPosition(room).y, 0f), Quaternion.identity);
+        }
     }
 
     void VisualizeDungeon()
     {
-        foreach (var connection in currentPreset.connections)
+        foreach (var connection in currentPreset.passages)
         {
             int roomIndex1 = connection.roomIndex1;
             int roomIndex2 = connection.roomIndex2;
@@ -61,12 +62,12 @@ public class DungeonGenerator : MonoBehaviour
 }
 
 [System.Serializable]
-public class Connection
+public class Passage
 {
     public int roomIndex1;
     public int roomIndex2;
 
-    public Connection(int roomIndex1, int roomIndex2)
+    public Passage(int roomIndex1, int roomIndex2)
     {
         this.roomIndex1 = roomIndex1;
         this.roomIndex2 = roomIndex2;
@@ -75,11 +76,13 @@ public class Connection
 [System.Serializable]
 public abstract class DungeonPreset
 {
-    public List<Connection> connections = new List<Connection>();
+    public List<Passage> passages = new List<Passage>();
+    public HashSet<int> roomsAvailable = new HashSet<int>();
     public int positionBossRoom = 27;
     public int positionBossTransitionRoom;
     public int positionUpgrade1;
     public int positionUpgrade2;
+
 }
 
 [System.Serializable]
@@ -91,24 +94,30 @@ public class Dungeon1 : DungeonPreset
         positionUpgrade1 = 13;
         positionUpgrade2 = 19;
 
-        connections.Add(new Connection(2, 3));
-        connections.Add(new Connection(2, 7));
-        connections.Add(new Connection(7, 6));
-        connections.Add(new Connection(6, 5));
-        connections.Add(new Connection(6, 11));
-        connections.Add(new Connection(7, 12));
-        connections.Add(new Connection(11, 12));
-        connections.Add(new Connection(12, 13));
-        connections.Add(new Connection(11, 16));
-        connections.Add(new Connection(16, 17));
-        connections.Add(new Connection(17, 22));
-        connections.Add(new Connection(22, 21));
-        connections.Add(new Connection(21, 20));
-        connections.Add(new Connection(22, 23));
-        connections.Add(new Connection(23, 24));
-        connections.Add(new Connection(24, 19));
-        connections.Add(new Connection(21, 26));
-        connections.Add(new Connection(26, 27));
+        passages.Add(new Passage(2, 3));
+        passages.Add(new Passage(2, 7));
+        passages.Add(new Passage(7, 6));
+        passages.Add(new Passage(6, 5));
+        passages.Add(new Passage(6, 11));
+        passages.Add(new Passage(7, 12));
+        passages.Add(new Passage(11, 12));
+        passages.Add(new Passage(12, 13));
+        passages.Add(new Passage(11, 16));
+        passages.Add(new Passage(16, 17));
+        passages.Add(new Passage(17, 22));
+        passages.Add(new Passage(22, 21));
+        passages.Add(new Passage(21, 20));
+        passages.Add(new Passage(22, 23));
+        passages.Add(new Passage(23, 24));
+        passages.Add(new Passage(24, 19));
+        passages.Add(new Passage(21, 26));
+        passages.Add(new Passage(26, 27));
+
+        foreach (var connection in passages)
+        {
+            roomsAvailable.Add(connection.roomIndex1);
+            roomsAvailable.Add(connection.roomIndex2);
+        }
     }
 }
 [System.Serializable]
@@ -120,19 +129,19 @@ public class Dungeon2 : DungeonPreset
         positionUpgrade1 = 0;
         positionUpgrade2 = 0;
 
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
-        connections.Add(new Connection(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
+        passages.Add(new Passage(0, 0));
     }
 }
