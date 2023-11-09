45f2e7c8c4c2ff4550cf8aa25c2511498cea0241
4CEspX <simon@jonassonmail.se>
2023-11-09 14:12:26 +0100

    asd

--- Assets/Scripts/A GAME/HeartUpdate.cs
+++ Assets/Scripts/A GAME/HeartUpdate.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class HeartUpdate : MonoBehaviour
 {
+    public GameObject PlayerObj;
+    Player player;
+
     //Prefabs
     public GameObject fullHeartIcon;
     public GameObject halfHeartIcon;
     public GameObject emptyHeartIcon;
+    void Start()
+    {
+        PlayerObj = GameObject.FindWithTag("Player");
+        player = PlayerObj.GetComponent<Player>();
+    }
     public void UpdateHearts()
     {
         foreach (Transform childObj in transform)
