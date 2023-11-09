f162709745676f5b3dfb9687c969448a50d92c22
4CEspX <simon@jonassonmail.se>
2023-11-09 14:07:33 +0100

    asd

--- Assets/Scripts/A GAME/HeartUpdate.cs
+++ Assets/Scripts/A GAME/HeartUpdate.cs
@@ -8,20 +8,12 @@ public class HeartUpdate : MonoBehaviour
     public GameObject fullHeartIcon;
     public GameObject halfHeartIcon;
     public GameObject emptyHeartIcon;
-
-    // Start is called before the first frame update
-    void Start()
+    public void UpdateHearts()
     {
         foreach (Transform childObj in transform)
         {
             Destroy(childObj.gameObject);
         }
         Instantiate(fullHeartIcon, gameObject.transform);
-    }
-
-    // Update is called once per frame
-    public void UpdateHearts()
-    {
-        
     }
 }
