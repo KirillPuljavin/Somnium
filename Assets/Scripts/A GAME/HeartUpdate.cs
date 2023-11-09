03768889ed090e7fbd7fba9f4995eac5606a4479
4CEspX <simon@jonassonmail.se>
2023-11-09 13:59:43 +0100

    HeartFixes

--- Assets/Scripts/A GAME/HeartUpdate.cs
+++ Assets/Scripts/A GAME/HeartUpdate.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeartUodate : MonoBehaviour
+public class HeartUpdate : MonoBehaviour
 {
+    //gameObjects
+    public GameObject heartsHUD;
+    
+    //Prefabs
+    public Prefab
+
     // Start is called before the first frame update
     void Start()
     {
-        // foreach (Transform childObj in heartsHUD.transform)
-        // {
-        //     Destroy(childObj.gameObject);
-        // }
-        // Instantiate(heartIcon, heartsHUD.gameObject.transform);
+        foreach (Transform childObj in heartsHUD.transform)
+        {
+            Destroy(childObj.gameObject);
+        }
+        Instantiate(heartIcon, heartsHUD.gameObject.transform);
     }
 
     // Update is called once per frame
-    void Update()
+    public void UpdateHearts()
     {
         
     }
