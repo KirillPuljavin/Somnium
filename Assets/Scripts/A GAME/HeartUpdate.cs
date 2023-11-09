524c2c6c9d143deff906d87e01e69288ec95fc62
4CEspX <simon@jonassonmail.se>
2023-11-09 14:39:49 +0100

    Hearts Fixed

--- Assets/Scripts/A GAME/HeartUpdate.cs
+++ Assets/Scripts/A GAME/HeartUpdate.cs
@@ -23,6 +23,87 @@ public class HeartUpdate : MonoBehaviour
             Destroy(childObj.gameObject);
         }
         if (player.Hearts % 2 == 0)
-            Instantiate(fullHeartIcon, gameObject.transform);
+        {
+            switch ((player.Hearts))
+            {
+                case (2):
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    break;
+                case (4):
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    break;
+                case (6):
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    break;
+                case (8):
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    break;
+                case (10):
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    break;
+
+            }
+        }
+        else
+        {
+            switch ((player.Hearts))
+            {
+                case (1):
+                    Instantiate(halfHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    break;
+                case (3):
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(halfHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    break;
+                case (5):
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(halfHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    break;
+                case (7):
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(halfHeartIcon, gameObject.transform);
+                    Instantiate(emptyHeartIcon, gameObject.transform);
+                    break;
+                case (9):
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(fullHeartIcon, gameObject.transform);
+                    Instantiate(halfHeartIcon, gameObject.transform);
+                    break;
+            }
+        }
     }
 }
