643908c22d3252b5b2079b745d5eebacf94cc074
4CEspX <simon@jonassonmail.se>
2023-11-10 15:27:26 +0100

    Enemy Anims

--- Assets/Scripts/Entities/EnemyBlob.cs
+++ Assets/Scripts/Entities/EnemyBlob.cs
@@ -5,26 +5,55 @@ using UnityEngine;
 
 public class EnemyBlob : MonoBehaviour
 {
-    Player Player;
+    public Rigidbody2D rb;
+    private Player player;
+    private Vector3 directionToPlayer;
+    private Vector3 localScale;
     public float speed;
     public float enemyHP = 8;
-
+    public Animator animator;
     private float oldSpeed;
     float attackCooldown = 0;
     float dashCooldown = 0;
+    
     void Start()
     {
-        Player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
         oldSpeed = speed;
+        rb = GetComponent<Rigidbody2D>();
+        localScale = transform.localScale;
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, new Vector2(Player.gameObject.transform.position.x, Player.gameObject.transform.position.y + 0.5f), speed * Time.deltaTime);
 
         // Cooldown
         if (attackCooldown < 1) attackCooldown += Time.deltaTime;
         if (dashCooldown > 0) dashCooldown -= Time.deltaTime;
+
+        
+
+        // animator.SetFloat("Horizontal", rb.velocity.x);
+        // animator.SetFloat("Vertical", rb.velocity.y);
+        // animator.SetFloat("Speed", rb.velocity.sqrMagnitude);
+        MoveEnemy();
+        Debug.Log(rb.velocity);
+    }
+    private void MoveEnemy()
+    {
+        directionToPlayer = (player.transform.position - transform.position).normalized;
+        rb.velocity = new Vector2(directionToPlayer.x, directionToPlayer.y) * speed;
+    }
+    private void LateUpdate()
+    {
+        if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(localScale.x, localScale.y, localScale.z);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-localScale.x, localScale.y, localScale.z);
+        }
     }
 
 
@@ -37,14 +66,15 @@ public class EnemyBlob : MonoBehaviour
         // Attack Player
         if (collider.gameObject.tag == "Player" && attackCooldown >= 1)
         {
-            if (!Player.isDashing)
+            if (!player.isDashing)
             {
-                Player.TakeDamage(1);
+                player.TakeDamage(1);
                 attackCooldown = 0;
+                animator.Play("Attack_Right");
             }
         }
         // Dash Take Damage
-        if (collider.gameObject.tag == "dashHitbox" && dashCooldown <= 0 && Player.isDashing)
+        if (collider.gameObject.tag == "dashHitbox" && dashCooldown <= 0 && player.isDashing)
         {
             TakeDamage();
             dashCooldown = 0.5f;
@@ -57,7 +87,7 @@ public class EnemyBlob : MonoBehaviour
 
     public void TakeDamage()
     {
-        enemyHP -= Player.damage;
+        enemyHP -= player.damage;
 
         if (enemyHP <= 0)
         {
