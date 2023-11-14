cb44f596dbfb5c1f2da36f0e8f675aaf1b5a24dc
WarMolee <142778225+WarMolee@users.noreply.github.com>
2023-11-14 13:13:14 +0100

    added boss Demo

---
+++ Assets/Scripts/Entities/BossCat.cs
@@ -0,0 +1,167 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BossCat : MonoBehaviour
+{
+
+    Player Player;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    public GameObject BasicRight;
+    public GameObject BasicLeft;
+    public GameObject BasicUp;
+    public GameObject BasicDown;
+
+    public int speed;
+    public float agroRange;
+    public float basicCooldown;
+    private float distance;
+
+    public bool inRange;
+    public bool spawned = false;
+    public bool inAnimation;
+    private float angle;
+
+
+
+    private float basicTimer;
+
+    void Start()
+    {
+        basicTimer = basicCooldown;
+
+        Player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        StartCoroutine(Begin());
+
+        // Hide all basic attacks
+        BasicRight.SetActive(false);
+        BasicLeft.SetActive(false);
+        BasicUp.SetActive(false);
+        BasicDown.SetActive(false);
+    }
+
+
+
+    UnityEngine.Vector3 previousPosition;
+    void Update()
+    {
+        if (basicTimer <= basicCooldown) basicTimer += Time.deltaTime;
+        if (spawned == true)
+        {
+            if (!inRange && !inAnimation)
+            {
+                transform.position = UnityEngine.Vector3.MoveTowards(transform.position, new UnityEngine.Vector2(Player.gameObject.transform.position.x, Player.gameObject.transform.position.y), speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = UnityEngine.Vector3.MoveTowards(transform.position, new UnityEngine.Vector2(Player.gameObject.transform.position.x, Player.gameObject.transform.position.y), 0);
+            }
+            StartCoroutine(Animations());
+            calcDistance();
+            calcDirection();
+        }
+    }
+
+    private IEnumerator Begin()
+    {
+        animator.SetBool("spawned", true);
+        yield return new WaitForSeconds(0.833f);
+        spawned = true;
+    }
+
+
+    private IEnumerator Animations()
+    {
+        UnityEngine.Vector3 currentPosition = transform.position;
+
+        float horizontalVelocity = (currentPosition.x - previousPosition.x) / Time.deltaTime;
+        float verticalVelocity = (currentPosition.y - previousPosition.y) / Time.deltaTime;
+
+        animator.SetFloat("Horizontal", verticalVelocity);
+        animator.SetFloat("Vertical", horizontalVelocity);
+        animator.SetFloat("speed", Mathf.Sqrt(horizontalVelocity * horizontalVelocity + verticalVelocity * verticalVelocity));
+
+        previousPosition = currentPosition;
+
+        if (distance <= agroRange)
+        {
+            inRange = true;
+            if (basicTimer >= basicCooldown)
+            {
+                if (angle < 30 && angle > -45)
+                {
+                    inAnimation = true;
+                    BasicRight.SetActive(true);
+                    animator.Play("BasicRight");
+                    basicTimer = 0;
+                    yield return new WaitForSeconds(0.917f);
+                    BasicRight.SetActive(false);
+                    inAnimation = false;
+                }
+                else if (angle > 145 && angle < 180 || angle > -180 && angle < -130)
+                {
+                    inAnimation = true;
+                    BasicLeft.SetActive(true);
+                    animator.Play("BasicLeft");
+                    basicTimer = 0;
+                    yield return new WaitForSeconds(0.917f);
+                    BasicLeft.SetActive(false);
+                    inAnimation = false;
+                }
+                else if (angle < 145 && angle > 30)
+                {
+                    inAnimation = true;
+                    BasicUp.SetActive(true);
+                    animator.Play("BasicUp");
+                    basicTimer = 0;
+                    yield return new WaitForSeconds(1.167f);
+                    BasicUp.SetActive(false);
+                    inAnimation = false;
+                }
+                else if (angle > -130 && angle < -45)
+                {
+                    inAnimation = true;
+                    BasicDown.SetActive(true);
+                    animator.Play("BasicDown");
+                    basicTimer = 0;
+                    yield return new WaitForSeconds(1.167f);
+                    BasicDown.SetActive(false);
+                    inAnimation = false;
+                }
+                else
+                {
+                    Debug.Log("Error");
+                }
+            }
+        }
+        else
+        {
+            inRange = false;
+        }
+    }
+
+    void calcDistance()
+    {
+        distance = UnityEngine.Vector2.Distance(transform.position, Player.gameObject.transform.position);
+    }
+
+
+    void calcDirection()
+    {
+        UnityEngine.Vector3 direction = new UnityEngine.Vector3(Player.gameObject.transform.position.x - transform.position.x, Player.gameObject.transform.position.y - transform.position.y + 0.5f, 0);
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision with player detected!");
+        }
+    }
+
+}
