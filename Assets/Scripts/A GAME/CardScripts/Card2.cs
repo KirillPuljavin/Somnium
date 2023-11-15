bdd03ebb9f5a6df626f45854b710869a7960c594
4CEspX <simon@jonassonmail.se>
2023-11-15 15:53:35 +0100

    Fixed Augments

--- Assets/Scripts/A GAME/CardScripts/Card2.cs
+++ Assets/Scripts/A GAME/CardScripts/Card2.cs
@@ -9,15 +9,18 @@ public class Card2 : MonoBehaviour
     private Player Player;
     public Animator animator;
     public Animation anim;
+    public ChestScript chest;
     void Start()
     {
         Player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        chest = GameObject.FindWithTag("Chest").GetComponent<ChestScript>();
     }
     void OnMouseDown()
     {
         Cards = GameObject.FindGameObjectsWithTag("Card");
         Player.speed += 1;
         Player.dashingPower += 4;
+        Player.Card2Picked = true;
         foreach (GameObject obj in Cards)
         {
             Destroy(obj);
