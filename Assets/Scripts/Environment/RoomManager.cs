daeae5f1e66aa40b0225779abd457caa4965cf41
Kirill Puljavin <105564583+KirillPuljavin@users.noreply.github.com>
2023-11-18 02:41:31 +0100

    SUPER FIX

--- Assets/Scripts/Environment/RoomManager.cs
+++ Assets/Scripts/Environment/RoomManager.cs
@@ -18,6 +18,7 @@ public class RoomManager : MonoBehaviour
     [SerializeField] private GameObject spiderEnemyPrefab;
     [SerializeField] private GameObject crafting1Prefab;
     [SerializeField] private GameObject crafting2Prefab;
+    [SerializeField] private GameObject chestPrefab;
     private Transform enemyParent;
 
     private static List<GameObject> Enemies = new List<GameObject>();
@@ -67,19 +68,21 @@ public class RoomManager : MonoBehaviour
             }
         }
 
+        // Spawn Chest
+        if (player.currRoom == dungeon.currentPreset.positionChest1 || player.currRoom == dungeon.currentPreset.positionChest2)
+        {
+            Instantiate(chestPrefab, currentRoom.transform.GetChild(3).GetChild(0).transform.position, Quaternion.identity, currentRoom.transform);
+        }
+
         // Spawn Crafting Station
         if (player.currRoom == dungeon.currentPreset.positionUpgrade1)
         {
             Instantiate(crafting1Prefab, currentRoom.transform.GetChild(4).GetChild(0).transform.position, Quaternion.identity, currentRoom.transform);
-            Debug.Log("Instantiated crafting 1 in room: " + dungeon.currentPreset.positionUpgrade1);
         }
         else if (player.currRoom == dungeon.currentPreset.positionUpgrade2)
         {
             Instantiate(crafting2Prefab, currentRoom.transform.GetChild(4).GetChild(0).transform.position, Quaternion.identity, currentRoom.transform);
-            Debug.Log("Instantiated crafting 2 in room: " + dungeon.currentPreset.positionUpgrade2);
         }
-
-        // Spawn Chest
     }
 
     public void EnemyDied() => StartCoroutine(CheckForEnemies());
@@ -88,12 +91,6 @@ public class RoomManager : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
 
         Enemies.Clear(); foreach (Transform enemy in enemyParent) Enemies.Add(enemy.gameObject);
-        Debug.Log("Enemy count: " + Enemies.Count);
-
-        if (Enemies.Count <= 0)
-        {
-            roomCleared = true;
-            Debug.Log("ROOM CLEARED");
-        }
+        if (Enemies.Count <= 0) roomCleared = true;
     }
 }
