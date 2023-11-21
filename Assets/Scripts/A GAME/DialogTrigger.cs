cc3b04280b3a4dcc3a207e68b9f2ed6160deccf0
Mizfra <vipkotenok939@gmail.com>
2023-11-21 14:56:27 +0100

    Tutorial room
    We shall pray

---
+++ Assets/Scripts/A GAME/DialogTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    public Dialog dialog;
+
+    public void TriggerDialog ()
+    {
+        FindObjectOfType<DialogManager>().StartDialog(dialog);
+    }
+    private void OnMouseDown()
+    {
+        TriggerDialog();
+    }
+}
