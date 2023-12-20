81de07fa3719f11aeccec2b54a4055d5b31b1ed0
Queereell <thelegendaryyouth@gmail.com>
2023-12-20 13:25:15 +0100

    credit

---
+++ Assets/Scripts/A GAME/creditsScroll.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class creditsScroll : MonoBehaviour
+{
+    float x = 0;
+    void Update()
+    {
+        transform.parent.parent.GetChild(1).gameObject.SetActive(false);
+        if (x < 20)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * 1.5f);
+            x += Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene("Main Meny");
+        }
+    }
+}
